97e71f0e118c213ab2a570ab50f6f721fc474bb4
Lucas Garcia <garcia@treibauf.ch>
2024-03-24 22:15:29 +0000

    adjusted mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,20 @@ export default function Home() {
       <div className="content flex flex-col items-center justify-evenly h-full">
         <Image src="/logo-lutefol.svg" width={246} height={32} alt="" />
         <div className="flex flex-col">
-          <h1 className="text-5xl">
+          <h1 className="text-4xl md:text-5xl">
             Revolutionizing sports <br /> technology for the future
           </h1>
-          <h2 className="text-2xl">
+          <h2 className="text-xl md:text-2xl">
             Experience the next level of sports innovation <br /> and performance. Join us today!
           </h2>
         </div>
-        <div className="flex gap-2 text-base">
+        <div className="flex flex-col md:flex-row gap-2 text-sm md:text-base">
           Are you looking for Piratas FC Oficcial Website?
           <Link href="https://piratasfc.vercel.app" className="underline" target="_blank">
             Visit Piratas FC Official Site
           </Link>
         </div>
-        <h3 className="text-base">
+        <h3 className="text-sm md:text-base">
           © 2024 Lutefol from Zassa
         </h3>
       </div>
